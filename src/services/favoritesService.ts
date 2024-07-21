8809163dacc04e6f64dccbc659398bfbcce38461
mrjordantanner <mrjordantanner@gmail.com>
2024-07-21 16:47:52 -0500

    Fix env vars

--- src/services/favoritesService.ts
+++ src/services/favoritesService.ts
@@ -2,7 +2,7 @@ import axios, { AxiosRequestConfig } from 'axios';
 import { FavoritesList, UserFavorites } from '../models/favorites';
 
 class FavoritesService {
-  private apiEndpoint = import.meta.env.API_URL || 'http://localhost:5000/api';
+  private apiEndpoint = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
   baseUrl = `${this.apiEndpoint}/api/favorites`;
   axiosOptions: AxiosRequestConfig = {
     headers: {
